0e13cc90992558e6499f7d50bde020f63afded5c
mazelx@pi <mazelx@pi>
2017-07-26 16:45:56 +0000

    access mongodb data through api

--- api.py
+++ api.py
@@ -1,35 +1,53 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, fields, marshal_with
+from pymongo import MongoClient
+import datetime
+import _strptime
 
 app = Flask(__name__)
 api = Api(app)
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
 
 surfchecks = {
     "201707261600": {'yo': 'yooooo'},
     "201707261530": {'he': 'heeeee'},
 }
 
-class SurfCheck(Resource):
-    def get(self, datetime):
-	return {datetime: surfchecks[datetime]}
 
-    def put(self, datetime):
-	print("data : " + request.form['data'])
-	surfchecks[datetime] = request.form['data']
-	return {datetime: surfchecks[datetime]}
+client = MongoClient()
+db = client.surf_check
+wave_data = db.wave_data
+
+wave_fields = {
+	"datetime": fields.DateTime(dt_format="iso8601"),
+        "water_temperature": fields.String,
+        "wave_direction": fields.String,
+        "wave_height": fields.String,
+        "wave_height_max": fields.String,
+        "wave_period": fields.String,
+        "wave_spreading": fields.String
+}
+
+
+class SurfCheck(Resource):
+    @marshal_with(wave_fields, envelope='wave_data')
+    def get(self, dt_value):
+	dt_max = datetime.datetime.strptime(dt_value, '%Y%m%d%H%M')
+	dt_min = dt_max - datetime.timedelta(minutes=30)
+	doc = wave_data.find_one({"datetime": {"$lte":dt_max, "$gt": dt_min}},{"_id": 0})
+	return doc
+
+#    def put(self, datetime):
+#	dt = datetime.strptime(datetime, '%Y%m%d%H%M')
+#	surfchecks[datetime] = request.form['data']
+#	return {datetime: surfchecks[datetime]}
 
 
 class SurfCheckList(Resource):
+    @marshal_with(wave_fields, envelope='wave_data')
     def get(self):
-        return surfchecks
+ 	return list(wave_data.find().sort([("datetime",0)]).limit(100))
 
-api.add_resource(SurfCheck, '/surfcheck/<string:datetime>')
+api.add_resource(SurfCheck, '/surfcheck/<string:dt_value>')
 api.add_resource(SurfCheckList, '/surfchecks')
 
 if __name__ == '__main__':

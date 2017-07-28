1dc83a304d7a9734f25d5ff1bf847c887b48e0b6
mazelx@pi <mazelx@pi>
2017-07-28 09:35:34 +0000

    prepare for double servers

--- api/surfcheck_api.py
+++ api/surfcheck_api.py
@@ -47,4 +47,4 @@ api.add_resource(SurfCheck, '/surfchecks/<string:dt_value>')
 api.add_resource(SurfCheckList, '/surfchecks')
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0')
+    app.run(debug=True, host= '0.0.0.0', port=5001)

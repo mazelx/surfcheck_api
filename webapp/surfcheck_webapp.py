b362f0088c6667dd6aed51bcbeb8843ad12f99a7
mazelx <mazelx@gmail.com>
2017-07-29 03:03:32 +0200

    remove api call from flask server

--- webapp/surfcheck_webapp.py
+++ webapp/surfcheck_webapp.py
@@ -5,17 +5,17 @@ import requests
 
 app = Flask(__name__)
 
-surfchecks_endpoint = 'http://localhost:5001/surfchecks'
+# surfchecks_endpoint = 'http://localhost:5001/surfchecks'
 
 
 @app.route("/")
 def surfcheck():
-    r = requests.get(surfchecks_endpoint)
-    if (r.status_code != 200):
-        raise "errrrr"
-    docs = r.json()['wave_data']
+    # r = requests.get(surfchecks_endpoint)
+    # if (r.status_code != 200):
+        # raise "errrrr"
+    # docs = r.json()['wave_data']
     # return render_template('surfcheck.html', titre="Surfcheck mate", docs=docs)
-    return render_template('surfcheck.html', titre="Surfcheck mate", docs=docs)
+    return render_template('surfcheck.html', titre="Surfcheck mate")
 
 
 if __name__ == '__main__':

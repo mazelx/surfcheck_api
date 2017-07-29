af30af1eb249162f98cf83cfe4735cce44d9e6e4
mazelx <mazelx@gmail.com>
2017-07-29 12:33:30 +0200

    disable debugging for api

--- api/surfcheck_api.py
+++ api/surfcheck_api.py
@@ -54,4 +54,4 @@ api.add_resource(SurfCheck, '/surfchecks/<string:dt_value>')
 api.add_resource(SurfCheckList, '/surfchecks')
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0', port=5001)
+    app.run(host= '0.0.0.0', port=5001)

f3b514b6a316ce63915eb79e4b0118f2ce15d87a
mazelx <mazelx@gmail.com>
2017-08-03 01:01:04 +0200

    refactor html

--- api/surfcheck_api.py
+++ api/surfcheck_api.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api, fields, marshal_with
 from pymongo import MongoClient
 import datetime
 import _strptime
-from flask.ext.cors import CORS
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ cors = CORS(app)
 client = MongoClient()
 db = client.surf_check
 wave_data = db.wave_data
+weather_data = db.weather_data
 
 wave_fields = {
         "datetime": fields.DateTime(dt_format="iso8601"),
@@ -27,13 +28,24 @@ wave_fields = {
 
 
 class SurfCheck(Resource):
-    @marshal_with(wave_fields, envelope='wave_data')
+    @marshal_with(wave_fields)
     def get(self, dt_value):
         if(dt_value == 'last'):
-            return wave_data.find().sort([("datetime",0)]).limit(1)[0]
-        dt_max = datetime.datetime.strptime(dt_value, '%Y%m%d%H%M')
-        dt_min = dt_max - datetime.timedelta(minutes=30)
-        doc = wave_data.find_one({"datetime": {"$lte":dt_max, "$gt": dt_min}},{"_id": 0})
+            return wave_data.find({}).sort("datetime", 1).limit(1)[0]
+        wave_dt_max = datetime.datetime.strptime(dt_value, '%Y%m%d%H%M')
+        wave_dt_min = wave_dt_max - datetime.timedelta(minutes=30)
+        wave_doc = wave_data.find_one({"datetime": {"$lte":wave_dt_max, "$gt": wave_dt_min}},{"_id": 0})
+        if(not wave_doc):
+            print("ko")
+            return {}
+        print("wave doc dt :" + str(wave_doc.get("datetime")))
+        weather_dt_max = wave_doc.get("datetime") + datetime.timedelta(minutes=15)
+        weather_dt_min = wave_doc.get("datetime") - datetime.timedelta(minutes=15)
+        weather_doc = weather_data.find_one({"datetime":{"$lte":weather_dt_max, "$gt": weather_dt_min}}, {"datetime":0})
+        print("wearther doc : " + str(weather_doc))
+        doc = wave_doc.copy()
+        doc.update(weather_doc)
+        doc = wave_doc
         return doc
 
 #    def put(self, datetime):
@@ -43,12 +55,13 @@ class SurfCheck(Resource):
 
 
 class SurfCheckList(Resource):
-    @marshal_with(wave_fields, envelope='wave_data')
+    @marshal_with(wave_fields)
     def get(self):
         dt_max = datetime.datetime.utcnow()
         dt_min = dt_max - datetime.timedelta(hours=49)
-        docs = wave_data.find({"datetime": {"$lte":dt_max, "$gt": dt_min}}).sort([("datetime",1)])
-        return list(docs)
+        wave_docs = wave_data.find({"datetime": {"$lte":dt_max, "$gt": dt_min}}).sort([("datetime",1)])
+        # weather_docs = wave
+        return list(wave_docs)
 
 api.add_resource(SurfCheck, '/surfchecks/<string:dt_value>')
 api.add_resource(SurfCheckList, '/surfchecks')
